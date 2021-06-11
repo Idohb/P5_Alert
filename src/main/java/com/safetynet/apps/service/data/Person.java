4965e7ad25b83ecc5f1e80303be7e6335be803fd
Cill <bodivann@gmail.com>
2021-06-11 16:16:19 +0200

    test private method + add Constructor to Person class

--- src/main/java/com/safetynet/apps/service/data/Person.java
+++ src/main/java/com/safetynet/apps/service/data/Person.java
@@ -1,5 +1,9 @@
 package com.safetynet.apps.service.data;
 
+import com.safetynet.apps.controller.PersonRequest;
+import com.safetynet.apps.mapper.MedicalRecordsConverter;
+import com.safetynet.apps.model.entity.MedicalRecordsEntity;
+import com.safetynet.apps.model.entity.PersonEntity;
 import lombok.Data;
 
 @Data
@@ -13,4 +17,20 @@ public class Person {
     private String zip;
     private String email;
     private MedicalRecords medicalRecords;
+
+    public Person() {}
+
+    public Person(PersonEntity p)  {
+        MedicalRecordsConverter medicalRecordsConverter = new MedicalRecordsConverter();
+        setId(p.getId());
+        setFirstName(p.getFirstName());
+        setLastName(p.getLastName());
+        setAddress(p.getAddress());
+        setCity(p.getCity());
+        setZip(p.getZip());
+        setEmail(p.getEmail());
+        setPhone(p.getPhone());
+        setMedicalRecords(medicalRecordsConverter.mapperMedicalRecords(p.getMedicalRecord()));
+    }
+
 }
--- src/test/java/com/safetynet/apps/unitTest/Person/PersonServiceTest.java
+++ src/test/java/com/safetynet/apps/unitTest/Person/PersonServiceTest.java
@@ -2,9 +2,11 @@ package com.safetynet.apps.unitTest.Person;
 
 import com.safetynet.apps.controller.PersonRequest;
 import com.safetynet.apps.mapper.PersonConverter;
+import com.safetynet.apps.model.entity.MedicalRecordsEntity;
 import com.safetynet.apps.model.entity.PersonEntity;
 import com.safetynet.apps.model.repository.PersonRepository;
 import com.safetynet.apps.service.PersonService;
+import com.safetynet.apps.service.data.MedicalRecords;
 import com.safetynet.apps.service.data.Person;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,84 +47,29 @@ class PersonServiceTest {
 
 
     @Test
-    void createPerson_ShouldReturnPersonExpected() {
-        Person person = new Person();
-        person.setId(1L);
+    void updateEntity_ShouldChangeEntityFromPersonrequest() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
 
-        PersonEntity personEntity = new PersonEntity();
-        personEntity.setFirstName("1");
-        personEntity.setZip("2");
-        personEntity.setPhone("3");
-        personEntity.setAddress("4");
-        personEntity.setCity("5");
-        personEntity.setEmail("6");
-        personEntity.setLastName("7");
+        //Given
+        Method method = PersonService.class.getDeclaredMethod("updateEntity", PersonEntity.class, PersonRequest.class);
 
-        when(personRepository.save(any())).thenReturn(personEntity);
+        PersonRequest personRequest = new PersonRequest("8","9","10","11","12","13","14");
 
+        MedicalRecordsEntity medicalRecord = new MedicalRecordsEntity();
+        PersonEntity entity = new PersonEntity(1L,"1","2","3","4","5","6","7",medicalRecord);
 
-        person = personConverter.mapperPerson( personEntity);
+        //When
+        method.setAccessible(true);
+        method.invoke(personService, entity, personRequest);
 
-        assertThat(person.getFirstName()).isEqualTo("1");
-        assertThat(person.getZip()).isEqualTo("2");
-        assertThat(person.getPhone()).isEqualTo("3");
-        assertThat(person.getAddress()).isEqualTo("4");
-        assertThat(person.getCity()).isEqualTo("5");
-        assertThat(person.getEmail()).isEqualTo("6");
-        assertThat(person.getLastName()).isEqualTo("7");
-
-
+        //Then
+        assertEquals(entity.getFirstName()  , "8");
+        assertEquals(entity.getLastName()   , "9");
     }
 
+    @Test
+    void updatePerson_ShouldChangeFirstName() {
 
-//    @Test
-//    void updatePerson_ShouldThrowExceptionOnMissingPerson() {
-//
-//    }
-//
-//    @Test
-//    void updatePerson_ShouldChangeFirstName() {
-//        PersonEntity personEntity = new PersonEntity();
-//        personEntity.setFirstName("titi");
-//
-//        Person person = new Person();
-//
-//        PersonRequest personRequest = new PersonRequest();
-//        personRequest.setFirstName("toto");
-//        personRequest.setLastName("tutu");
-//
-//        when(personRepository.findById(1L)).thenReturn(Optional.of(personEntity));
-//        when(personRepository.save(any())).thenReturn(null);
-////        when(personConverter.mapperPerson(any())).thenReturn(null);
-//
-//        personService.updatePerson(1L, personRequest);
-//
-//        assertEquals(personEntity.getFirstName(), "AZERTY");
-//        assertEquals(personEntity.getLastName(), "AQWZSX");
-//
-//
-//    }
-
-
-//    @Test
-//    void updatePerson_ShouldChangeFirstName() {
-//        MedicalRecords medicalRecords = new MedicalRecords();
-//        Person person = new Person(1L,"1","2","3","4","5","6","7",medicalRecords/*, fireStation*/);
-//        person.setFirstName("AZERTY");
-//
-//        PersonDTO personToUpdate = new PersonDTO(person);
-//
-//        personToUpdate.setFirstName("QWERTY");
-//        personToUpdate.setLastName("AQWZSX");
-//
-//        when(personRepository.findById(1L).map(p -> new PersonDTO(p)).get()).thenReturn(personToUpdate);
-//        when(personRepository.save(any())).thenReturn(null);
-//
-//        personService.updatePerson(1L, personToUpdate);
-//
-//        assertEquals(personToUpdate.getFirstName(), "AZERTY");
-//        assertEquals(personToUpdate.getLastName(), "AQWZSX");
-//    }
+    }
 
 //    @Test
 //    void updatePerson_ShouldChangeLastName() {
